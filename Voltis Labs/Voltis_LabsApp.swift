027d0f728e03a92e1f4f76838d580c84d3c11c83
colloidOS <nnagoziemcollin@gmail.com>
2025-02-20 22:25:45 +0100

    Initial Commit

---
+++ Voltis Labs/Voltis_LabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Voltis_LabsApp.swift
+//  Voltis Labs
+//
+//  Created by Macintosh HD on 20/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Voltis_LabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
